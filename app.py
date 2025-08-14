5954eedb7db328c3a78a9950600f8ba0e1a5a913
prince <masterprincescs430@gmail.com>
2025-08-14 21:17:01 +0530

    initail

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request,redirect,  url_for,session, Response
+
+app=Flask(__name__)
+app.secret_key="supersecret"
+
+@app.route("/",methods=["GET","POST"])
+def login():
+    if(request.method=="POST"):
+        username=request.form.get("username")
+        password=request.form.get("password")
+
+        if username=="admin"and password=="123":
+            session["user"]=username
+            return redirect(url_for("welcome"))
+        else:
+            return Response("In- valid credentials. Try again", mimetype="text/plain")
+
+
+    return '''
+            <h2>Login Page</h2>
+            <form method="POST">
+            Username:<input type ="text" name="username"><br>
+            Password:<input type="text" name="password"></br>
+            <input type="Submit" value="Login">
+            </form>
+        '''
+
+
+@app.route("/welcome")
+def welcome():
+    if "user" in session:
+        return f'''
+        <h2>Welcome,{session['user']}!</h2>
+        <a href="{url_for('logout')}">logout</a>
+        '''
+    return redirect (url_for("login"))  
+
+
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
+
+
+if __name__ == "__main__":
+   app.run(debug=True)
+
